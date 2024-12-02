fe7eb38f2dfb5efc3ea3fddd786b52e89139db36
Aljhon07 <ciprianoaljhon07@gmail.com>
2024-12-02 19:32:17 +0800

    Add global styles, color themes, and endpoints constants

---
+++ Lingua/src/constants/globalStyles.js
@@ -0,0 +1,22 @@
+export const spacing = {
+  sm: 4,
+  md: 8,
+  lg: 16,
+  xl: 20,
+  xxl: 36,
+  xxxl: 48,
+};
+
+export const border = {
+  sm: 4,
+  md: 8,
+  lg: 16,
+};
+
+export const fontSize = {
+  sm: 14,
+  base: 16,
+  md: 20,
+  lg: 24,
+  xl: 32,
+};
