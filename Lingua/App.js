a7d242e0e48a8483e7def019f55aaddde4c5ae18
Aljhon07 <ciprianoaljhon07@gmail.com>
2024-12-02 23:59:06 +0800

    Refactor: Move NavigationContainer to ThemedApp component

--- Lingua/App.js
+++ Lingua/App.js
@@ -1,8 +1,10 @@
 import "@utils/gesture-handler.native"
 import React from "react"
+import { NavigationContainer } from "@react-navigation/native"
 import ThemeProvider from "@context/ThemeProvider"
 import FontProvider from "@context/FontProvider"
 import AuthProvider from "@context/AuthProvider.jsx"
+import RootNavigator from "@navigation/RootNavigator"
 import ThemedApp from "@components/layouts/ThemedApp"
 
 export default function App() {
--- Lingua/src/components/layouts/ThemedApp.jsx
+++ Lingua/src/components/layouts/ThemedApp.jsx
@@ -1,13 +1,23 @@
-import { NavigationContainer } from "@react-navigation/native"
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native"
 import RootNavigator from "@navigation/RootNavigator"
-import ThemedView from "@components/atoms/ThemedView"
+import { useThemeContext } from "@context/ThemeProvider"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function ThemedApp() {
+  const { colors } = useThemeContext()
+
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: colors.background,
+    },
+  }
   return (
-    <ThemedView>
-      <NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer theme={MyTheme}>
         <RootNavigator />
       </NavigationContainer>
-    </ThemedView>
+    </SafeAreaView>
   )
 }
