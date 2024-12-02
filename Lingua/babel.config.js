17e864ee2d023edf256b460bc3bab2911dbb94d1
Aljhon07 <ciprianoaljhon07@gmail.com>
2024-12-02 19:04:56 +0800

    app initial setup

---
+++ Lingua/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // presets: ["module:metro-react-native-babel-preset"],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "@assets": "./assets",
+          "@components": "./src/components",
+          "@constants": "./src/constants",
+          "@context": "./src/context",
+          "@hooks": "./src/hooks",
+          "@buttons": "./src/components/molecules/buttons",
+          "@utils": "./src/utils",
+          "@services": "./src/services",
+          "@navigation": "./src/screens/navigation",
+        },
+      },
+    ],
+  ],
+}
