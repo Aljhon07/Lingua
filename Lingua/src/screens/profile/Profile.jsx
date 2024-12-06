b5c3af4a2621d26bd0aea0fde9b217ef4e236483
Aljhon07 <ciprianoaljhon07@gmail.com>
2024-12-06 23:30:50 +0800

    refactor: update navigation structure

--- Lingua/src/screens/profile/Profile.jsx
+++ Lingua/src/screens/profile/Profile.jsx
@@ -1,13 +1,20 @@
+import { CustomButton } from "@components/atoms/CustomButton"
 import { Loading } from "@components/atoms/Loading"
+import { useAuthContext } from "@context/AuthProvider"
+import { useThemeContext } from "@context/ThemeProvider"
+import { useToggle } from "@hooks/useToggle"
 import { getProfile } from "@services/directus/rest"
 import { useEffect, useState } from "react"
-import { Text } from "react-native"
+import { Button, Text, useTheme } from "react-native-paper"
 import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function Profile() {
   const [profile, setProfile] = useState(null)
   const [loading, setLoading] = useState(true)
-
+  const [val, toggleVal] = useToggle(false)
+  const { signOut } = useAuthContext()
+  const { toggleTheme } = useThemeContext()
+  const { colors } = useTheme()
   useEffect(() => {
     const fetchProfile = async () => {
       try {
@@ -27,6 +34,12 @@ export default function Profile() {
   return (
     <SafeAreaView>
       <Text>Profile</Text>
+      <Button mode="contained" onPress={signOut}>
+        Sign Out
+      </Button>
+      <CustomButton primary onPress={toggleTheme}>
+        {val.toString()}
+      </CustomButton>
       {profile ? (
         <Text>{profile.first_name}</Text>
       ) : (
--- Lingua/src/screens/translator/Translator.jsx
+++ Lingua/src/screens/translator/Translator.jsx
@@ -1,10 +1,10 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native-paper"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 export default function Translator() {
   return (
     <SafeAreaView>
       <Text>Translator</Text>
     </SafeAreaView>
-  );
+  )
 }
